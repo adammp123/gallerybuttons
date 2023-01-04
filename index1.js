4acc1fe2147f310786037067b841190a9a48e152
AdamP <adammp123@gmail.com>
2023-01-04 01:20:59 -0600

    changed background and box1 images

--- index1.js
+++ index1.js
@@ -1,5 +1,5 @@
 function buttonOne(){
-    document.querySelector('.box1').style.backgroundImage = "url(imgs/img2.jpg";
+    document.querySelector('.box1').style.backgroundImage = "url(imgs/earth.jpg";
     document.querySelector('.button-R1').style.display = 'none';
     document.querySelector('.button-R2').style.display = 'flex';
     document.querySelector('.button-L1').style.display = 'none';
@@ -8,7 +8,7 @@ function buttonOne(){
 }
 
 function buttonTwo(){
-    document.querySelector('.box1').style.backgroundImage = "url(imgs/img3.jpg";
+    document.querySelector('.box1').style.backgroundImage = "url(imgs/water.jpg";
     document.querySelector('.button-L3').style.display = 'flex';
     document.querySelector('.button-L2').style.display = 'none';
     document.querySelector('.button-L1').style.display = 'none';
@@ -16,7 +16,7 @@ function buttonTwo(){
 }
 
 function buttonOneL(){
-    document.querySelector('.box1').style.backgroundImage = "url(imgs/img1.jpg";
+    document.querySelector('.box1').style.backgroundImage = "url(imgs/fire.jpg";
     document.querySelector('.button-R1').style.display = 'flex';
     document.querySelector('.button-R2').style.display = 'none';
     document.querySelector('.button-L3').style.display = 'none';
